83024d5b9e2e09488dc9126b866b1f9a032bf8ac
Ryan Turner <c15ryan.turner@usafa.edu>
2013-11-05 11:12:56 -0700

    First commit of skeleton code

---
+++ LCD_Driver.h
@@ -0,0 +1,28 @@
+/*
+ * LCD_Driver.h
+ *
+ *  Created on: Oct 23, 2013
+ *      Author: C15Michael.Bentley
+ */
+
+#ifndef LCD_DRIVER_H_
+#define LCD_DRIVER_H_
+
+void initSPI();
+
+void initLCD();
+
+void LCDclear();
+
+void setCursorLine1();
+
+void setCursorLine2();
+
+void writeChar(char singleChar);
+
+void writeString(char*string);
+
+void scrollString(char*string1, char*string2, int screenSizeInChars);
+
+
+#endif /* LCD_DRIVER_H_ */
