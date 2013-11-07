47039914b6dd6b8edb16112206f7616c9605e68a
Ryan Turner <c15ryan.turner@usafa.edu>
2013-11-07 14:40:53 -0700

    Builds with no errors, prints player to LCD, and prints game over, but directions aren't working correctly.

--- game.c
+++ game.c
@@ -1,6 +1,7 @@
 #include <msp430g2553.h>
 #include "game.h"
 #include "LCD_Driver.h"
+#include "button.h"
 
 unsigned char initPlayer() {
 	return 0x80;
@@ -18,55 +19,71 @@ void clearPlayer(unsigned char player) {
 
 unsigned char movePlayer(unsigned char player, unsigned char direction) {
 	switch (direction) {
-	//
-	// update player position based on direction of movement
-	//
+	 //
+	 // update player position based on direction of movement
+	 //
 
-	case LEFT:
-		clearPlayer(player);
-		player -= 1;
-		if (player == 0x7F) {
-			player = 0x80;
-		}
-		if (player == 0xBF) {
-			player = 0xC0;
-		}
-		printPlayer(player);
-		waitForP1ButtonRelease(BIT1);
-		break;
+	 case LEFT:
+	 if (player != 0x80 && player != 0xC0) {
+	 player --;
+	 }
+	 break;
 
-	case RIGHT:
-		clearPlayer(player);
-		player += 1;
-		if (player == 0x88) {
-			player = 0x87;
-		}
-		printPlayer(player);
-		waitForP1ButtonRelease(BIT2);
-		break;
+	 case RIGHT:
+	 if ((player != 0x87) && (player != 0xC7)) {
+	 player ++;
+	 }
+	 break;
 
-	case UP:
-		clearPlayer(player);
-		if (player >= 0xC0) {
-			player -= 0x40;
-		}
-		printPlayer(player);
-		waitForP1ButtonRelease(BIT3);
-		break;
+	 case UP:
+	 if (player > 0x87) {
+	 player -= 0x40;
+	 }
+	 break;
 
-	case DOWN:
-		clearPlayer(player);
-		if (player <= 0x87) {
-			player += 0x40;
-		}
-		printPlayer(player);
-		waitForP1ButtonRelease(BIT4);
-		break;
-	}
+	 case DOWN:
+	 if (player < 0xC0) {
+	 player += 0x40;
+	 }
+	 break;
+	 }
 
-	return player;
-}
+	 return player;
+	 }
+
+	 unsigned char didPlayerMove(unsigned char player) {
+	 if (isP1ButtonPressed(BIT1)) {
+	 clearPlayer(player);
+	 player = movePlayer(player, LEFT);
+	 printPlayer(player);
+	 TAR = 0;
+	 waitForP1ButtonRelease(BIT1);
+	 debounce();
+	 } else if (isP1ButtonPressed(BIT2)) {
+	 clearPlayer(player);
+	 player = movePlayer(player, RIGHT);
+	 printPlayer(player);
+	 TAR = 0;
+	 waitForP1ButtonRelease(BIT2);
+	 debounce();
+	 } else if (isP1ButtonPressed(BIT3)) {
+	 clearPlayer(player);
+	 player = movePlayer(player, UP);
+	 printPlayer(player);
+	 TAR = 0;
+	 waitForP1ButtonRelease(BIT3);
+	 debounce();
+	 } else if (isP1ButtonPressed(BIT4)) {
+	 clearPlayer(player);
+	 player = movePlayer(player, DOWN);
+	 printPlayer(player);
+	 TAR = 0;
+	 waitForP1ButtonRelease(BIT4);
+	 debounce();
+	 }
+	 return player;
+	 }
 
 char didPlayerWin(unsigned char player) {
-	return player == 0xC7;
-}
+		return player == 0xC7;
+	}
--- main.c
+++ main.c
@@ -2,45 +2,64 @@
 #include "game.h"
 #include "button.h"
 #include "LCD_Driver.h"
-#include "rand.h"
+//#include "rand.h"
 
 void init_timer();
 void init_buttons();
 
-int main(void)
-{
-        WDTCTL = (WDTPW|WDTHOLD);
+int TIMER = 0;
+int LOSE = 0;
+int GAMEOVER = 0;
 
-        unsigned char player = initPlayer();
+int main(void) {
+	WDTCTL = (WDTPW | WDTHOLD);
 
-        init_timer();
-        init_buttons();
-        __enable_interrupt();
-        initSPI();
-        initLCD();
+	unsigned char player = initPlayer();
 
-        while(1)
-        {
-                /*
-                 * while (game is on)
-                 * {
-                 *                 check if button is pushed (you don't want to block here, so don't poll!)
-                 *                 if button is pushed:
-                 *                         clear current player marker
-                 *                         update player position based on direction
-                 *                         print new player
-                 *                         clear two second timer
-                 *                         wait for button release (you can poll here)
-                 * }
-                 *
-                 * print game over or you won, depending on game result
-                 *
-                 * wait for button press to begin new game (you can poll here)
-                 * wait for release before starting again
-                 */
-        }
+	init_timer();
+	init_buttons();
+	__enable_interrupt();
+	initSPI();
+	initLCD();
 
-        return 0;
+	while (1) {
+		player = didPlayerMove(player);
+		if (LOSE == 1) {
+			LCDclear();
+			writeString("GAME");
+			setCursorLine2();
+			writeString("OVER");
+			setCursorLine1();
+			GAMEOVER = 1;
+			waitForP1ButtonRelease(BIT1 | BIT2 | BIT3 | BIT4);
+			debounce();
+		}
+		if (didPlayerWin(player)) {
+			LCDclear();
+			writeString("YOU");
+			setCursorLine2();
+			writeString("WON");
+			setCursorLine1();
+			GAMEOVER = 1;
+			waitForP1ButtonRelease(BIT1 | BIT2 | BIT3 | BIT4);
+			debounce();
+		}
+		if (GAMEOVER) {
+			char buttonsToPoll[4] = { BIT1, BIT2, BIT3, BIT4 };
+			while (!pollP1Buttons(buttonsToPoll, 4)) {
+				//poll until something is pressed
+			}
+			TAR = 0;
+			LOSE = 0;
+			TIMER = 0;
+			GAMEOVER = 0;
+			LCDclear();
+			player = initPlayer();
+			printPlayer(player);
+		}
+	}
+
+	return 0;
 }
 
 //
@@ -48,41 +67,38 @@ int main(void)
 //
 
 #pragma vector=TIMER0_A1_VECTOR
-__interrupt void TIMER0_A1_ISR()
-{
-    TACTL &= ~TAIFG;            // clear interrupt flag
-    TIMER += 1;
-    if (TIMER == 4){
-            LOSE = 1;
-    }
+__interrupt void TIMER0_A1_ISR() {
+	TACTL &= ~TAIFG;            // clear interrupt flag
+	TIMER += 1;
+	if (TIMER == 4) {
+		LOSE = 1;
+	}
 }
 
-void init_timer()
-{
+void init_timer() {
 	// do timer initialization work
-	        TACTL &= ~(MC1|MC0);        // stop timer
+	TACTL &= ~(MC1 | MC0);        // stop timer
 
-	        TACTL |= TACLR;             // clear TAR
+	TACTL |= TACLR;             // clear TAR
 
-	        TACTL |= TASSEL1;           // configure for SMCLK - what's the frequency (roughly)?
+	TACTL |= TASSEL1;   // configure for SMCLK - what's the frequency (roughly)?
 
-	        TACTL |= ID1|ID0;           // divide clock by 8 - what's the frequency of interrupt?
+	TACTL |= ID1 | ID0; // divide clock by 8 - what's the frequency of interrupt?
 
-	        TACTL &= ~TAIFG;            // clear interrupt flag
+	TACTL &= ~TAIFG;            // clear interrupt flag
 
-	        TACTL |= MC1;               // set count mode to continuous
+	TACTL |= MC1;               // set count mode to continuous
 
-	        TACTL |= TAIE;              // enable interrupt
+	TACTL |= TAIE;              // enable interrupt
 
-	        __enable_interrupt();       // enable maskable interrupts
+	__enable_interrupt();       // enable maskable interrupts
 
 }
 
-void init_buttons()
-{
-	// do button initialization work
-	        configureP1PinAsButton(BIT1|BIT2|BIT3|BIT4);
-	        P1IE |= BIT1|BIT2|BIT3|BIT4;                 // enable the interrupts
-	        P1IES |= BIT1|BIT2|BIT3|BIT4;                   // configure interrupt to sense falling edges
+void init_buttons() {
+														// do button initialization work
+	configureP1PinAsButton(BIT1 | BIT2 | BIT3 | BIT4);
+	P1IE |= BIT1 | BIT2 | BIT3 | BIT4;                  // enable the interrupts
+	P1IES |= BIT1 | BIT2 | BIT3 | BIT4; 				// configure interrupt to sense falling edges
 
 }
