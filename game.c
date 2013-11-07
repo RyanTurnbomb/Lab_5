a35606cc693f15a7029fed5becc516b536056ef2
Ryan Turner <c15ryan.turner@usafa.edu>
2013-11-06 17:34:32 -0700

    created initial case statements for the direction the player will move and declared the pins that each movement corresponds to.

--- game.c
+++ game.c
@@ -2,35 +2,57 @@
 #include "game.h"
 #include "LCD_Driver.h"
 
-unsigned char initPlayer()
-{
-        return 0x80;
+unsigned char initPlayer() {
+	return 0x80;
 }
 
-void printPlayer(unsigned char player)
-{
-        writeCommandByte(player);
-        writeDataByte('*');
+void printPlayer(unsigned char player) {
+	writeCommandByte(player);
+	writeDataByte('*');
 }
 
-void clearPlayer(unsigned char player)
-{
-        writeCommandByte(player);
-        writeDataByte(' ');
+void clearPlayer(unsigned char player) {
+	writeCommandByte(player);
+	writeDataByte(' ');
 }
 
-unsigned char movePlayer(unsigned char player, unsigned char direction)
-{
-        switch (direction) {
-                //
-                // update player position based on direction of movement
-                //
-        }
+unsigned char movePlayer(unsigned char player, unsigned char direction) {
+	switch (direction) {
+	case LEFT:
+		clearPlayer(player);
 
-        return player;
+		printPlayer(player);
+		waitForP1ButtonRelease(BIT1);
+		break;
+
+	case RIGHT:
+		clearPlayer(player);
+
+		printPlayer(player);
+		waitForP1ButtonRelease(BIT2);
+		break;
+
+	case UP:
+		clearPlayer(player);
+
+		printPlayer(player);
+		waitForP1ButtonRelease(BIT3);
+		break;
+
+	case DOWN:
+		clearPlayer(player);
+
+		printPlayer(player);
+		waitForP1ButtonRelease(BIT4);
+		break;
+		//
+		// update player position based on direction of movement
+		//
+	}
+
+	return player;
 }
 
-char didPlayerWin(unsigned char player)
-{
-        return player == 0xC7;
+char didPlayerWin(unsigned char player) {
+	return player == 0xC7;
 }
