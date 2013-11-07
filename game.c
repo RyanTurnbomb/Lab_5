ecb208ed6025cf32e7d757e9ace409e4bed21d57
Ryan Turner <c15ryan.turner@usafa.edu>
2013-11-07 10:43:09 -0700

    Filled in the case statements for the directions

--- game.c
+++ game.c
@@ -18,36 +18,50 @@ void clearPlayer(unsigned char player) {
 
 unsigned char movePlayer(unsigned char player, unsigned char direction) {
 	switch (direction) {
+	//
+	// update player position based on direction of movement
+	//
+
 	case LEFT:
 		clearPlayer(player);
-
+		player -= 1;
+		if (player == 0x7F) {
+			player = 0x80;
+		}
+		if (player == 0xBF) {
+			player = 0xC0;
+		}
 		printPlayer(player);
 		waitForP1ButtonRelease(BIT1);
 		break;
 
 	case RIGHT:
 		clearPlayer(player);
-
+		player += 1;
+		if (player == 0x88) {
+			player = 0x87;
+		}
 		printPlayer(player);
 		waitForP1ButtonRelease(BIT2);
 		break;
 
 	case UP:
 		clearPlayer(player);
-
+		if (player >= 0xC0) {
+			player -= 0x40;
+		}
 		printPlayer(player);
 		waitForP1ButtonRelease(BIT3);
 		break;
 
 	case DOWN:
 		clearPlayer(player);
-
+		if (player <= 0x87) {
+			player += 0x40;
+		}
 		printPlayer(player);
 		waitForP1ButtonRelease(BIT4);
 		break;
-		//
-		// update player position based on direction of movement
-		//
 	}
 
 	return player;
