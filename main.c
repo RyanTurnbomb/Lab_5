e3a55c0d53ba8b7e1b81d65ae0f8bb9c7c6dc281
Ryan Turner <c15ryan.turner@usafa.edu>
2013-11-06 18:14:44 -0700

    Included the Timer A ISR and the timer and button initializations

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "game.h"
 #include "button.h"
 #include "LCD_Driver.h"
+#include "rand.h"
 
 void init_timer();
 void init_buttons();
@@ -15,6 +16,8 @@ int main(void)
         init_timer();
         init_buttons();
         __enable_interrupt();
+        initSPI();
+        initLCD();
 
         while(1)
         {
@@ -44,12 +47,42 @@ int main(void)
 // YOUR TIMER A ISR GOES HERE
 //
 
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void TIMER0_A1_ISR()
+{
+    TACTL &= ~TAIFG;            // clear interrupt flag
+    TIMER += 1;
+    if (TIMER == 4){
+            LOSE = 1;
+    }
+}
+
 void init_timer()
 {
+	// do timer initialization work
+	        TACTL &= ~(MC1|MC0);        // stop timer
+
+	        TACTL |= TACLR;             // clear TAR
+
+	        TACTL |= TASSEL1;           // configure for SMCLK - what's the frequency (roughly)?
+
+	        TACTL |= ID1|ID0;           // divide clock by 8 - what's the frequency of interrupt?
+
+	        TACTL &= ~TAIFG;            // clear interrupt flag
+
+	        TACTL |= MC1;               // set count mode to continuous
+
+	        TACTL |= TAIE;              // enable interrupt
+
+	        __enable_interrupt();       // enable maskable interrupts
 
 }
 
 void init_buttons()
 {
+	// do button initialization work
+	        configureP1PinAsButton(BIT1|BIT2|BIT3|BIT4);
+	        P1IE |= BIT1|BIT2|BIT3|BIT4;                 // enable the interrupts
+	        P1IES |= BIT1|BIT2|BIT3|BIT4;                   // configure interrupt to sense falling edges
 
 }
